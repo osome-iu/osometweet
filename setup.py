b72514d677284b8f4adec9577e37bd0c712943ad
Kaicheng Yang <yangkc@iu.edu>
2021-02-09 09:12:42 -0500

    bump the version to 0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setup(
     name="osometweet",
-    version="0.2",
+    version="0.3",
     description="OSoMe Twitter library for academic researchers",
     long_description=long_description,
     long_description_content_type="text/markdown",
