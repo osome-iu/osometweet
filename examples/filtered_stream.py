30246f1c17fbf51baa1bb4c814096b233e57b29e
Matthew R. DeVerna <48068852+mr-devs@users.noreply.github.com>
2021-08-21 12:09:39 -0400

    shortened lines, doc strings + comments, JSONDecodeError

--- examples/filtered_stream.py
+++ examples/filtered_stream.py
@@ -1,73 +1,83 @@
 #!/usr/bin/env python3
 
 # Script Information
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 PURPOSE:
-    -Script to stream tweets with Twitter V2's sample stream endpoint 
+    -Script to stream tweets with Twitter V2's filtered stream endpoint.
 INPUT:
     - None
 OUTPUT:
     - tweet_data--{todays-date}.json : a file where each line
     represents one tweet
     - tweet_errors--{todays-date}.json : a file which records any
-    errors received (one per line). You can then learn why certain ids
-    were not returned.
+    errors received (one per line).
 Author: Christopher Torres-Lugo
 """
 
 
 # Import packages
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import os
-import sys
 import json
-import osometweet
 from datetime import datetime as dt
+
+import osometweet
 
 
 # Create Functions.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def load_bearer_token():
     """Load Twitter Keys from Local Environment."""
 
-    # To set your enviornment variables in your terminal execute a command like the
-    # one that you see below.
+    # To set your environment variables in your terminal execute a command like
+    # the one that you see below.
 
     # Example:
     # export 'TWITTER_BEARER_TOKEN'='<your_twitter_bearer_token>'
 
-    # Do this for all of your tokens, and then load them with the commands below,
-    # matching the string in the .get("string") to the name you chosen to the
-    # left of the equal sign above.
+    # Do this for all of your tokens, and then load them with the commands
+    # below, matching the string in the .get("string") to the name you've
+    # chosen to the left of the equal sign above.
 
     # Set Twitter tokens/keys.
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
 
     return bearer_token
 
 
 def stream_tweets(bearer_token):
+    """
+    Stream tweets from twitter and write them directly to a new line
+    delimited JSON file, named with today's date in "%Y-%m-%d_%H-%M" format.
+
+    Parameters
+    ----------
+    - bearer_token (str) : Twitter V2 bearer token.
+    """
     print("Streaming tweets...")
 
-    oauth2 = osometweet.OAuth2(bearer_token=bearer_token, manage_rate_limits=False)
+    oauth2 = osometweet.OAuth2(
+        bearer_token=bearer_token,
+        manage_rate_limits=False
+    )
     ot = osometweet.OsomeTweet(oauth2)
 
     # Add all tweet fields
     all_tweet_fields = osometweet.TweetFields(everything=True)
-    
+
     # Add streaming rules
-    rules = [{"value":"coronavirus", "tag":"all coronavirus tweets"},
-             {"value":"indiana", "tag":"all indiana tweets"}]
+    rules = [{"value": "coronavirus", "tag": "all coronavirus tweets"},
+             {"value": "indiana", "tag": "all indiana tweets"}]
     add_rules = {"add": rules}
     response = ot.set_filtered_stream_rule(rules=add_rules)
     print(f"API response from adding two rules: {response}")
-    
+
     # Retrieve active streaming rules
     current_rules = ot.get_filtered_stream_rule()
     print(f'The current filtered stream rules are: {current_rules}')
-    
+
     # Remove a streaming rule
     for data in current_rules.get('data'):
         if data['tag'] == 'all indiana tweets':
@@ -84,21 +94,21 @@ def stream_tweets(bearer_token):
         # stream is a Generator
         stream = ot.filtered_stream(fields=all_tweet_fields)
         # We have to iterate over the stream to fetch streamed tweets
-        for tweet in stream.iter_lines():   
+        for tweet in stream.iter_lines():
             # Get data and errors
             try:
                 data = json.loads(tweet).get("data")
 
-                # No matter what `data` and `errors` will return something, however,
-                # they may return `None` (i.e. no data/errors), which will throw a TypeError.
+                # When data is found, we write it to the open file
                 if data:
                     json.dump(data, data_file)
                     data_file.write("\n")
-            except JSONDecodeError:
+            except json.JSONDecodeError:
                 pass
 
+
 # Execute the program
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == "__main__":
     bearer_token = load_bearer_token()
     print(bearer_token)
